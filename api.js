8fc358be46a3b5818104fbbff51ed4826ed47765
fddafa <94169848+MCHUB-Trash@users.noreply.github.com>
2026-08-06 22:35:58 +0700

    Create api.js

---
+++ api.js
@@ -0,0 +1,66 @@
+return {
+	_credits = {
+		_icy = "xglacial",
+		_hexr = "xyz.hexr",
+		_xred = "Xred#6001",
+		_img = "9329990575"
+	},
+
+	_event_connection = {
+
+		_HALLOWEEN_CONNECTION = {
+			_halloween = false
+		},
+
+		_CHRISTMAS_CONNECTION = {
+			_christmas = false
+		}
+	},
+
+	_ADOPT_ME_API = {
+		_main_connected = false,
+
+		_adopt_me_halloween = {
+			_minimage_1 = nil,
+			_minigame_2 = nil,
+			_wrnt = true,
+			_wrnms = "Many exploiters get banned during the Halloween event, be sure to use your alt account."
+		},
+
+		_adopt_me_christmas = {
+			_minimage_1 = nil,
+			_minigame_2 = nil,
+			_wrnt = true,
+			_wrnms = "Many exploiters get banned during the Christmas event, be sure to use your alt account."
+		}
+	},
+
+	_rbxev_mode = {
+		_normal_mode = {
+			_ttl = Color3.fromRGB(255, 255, 255),
+			_bps = Color3.fromRGB(0, 255, 255),
+			_lblt = Color3.fromRGB(0, 255, 255),
+			_te = tostring("")
+		},
+
+		-- 5 October 2023
+		_halloween_mode = {
+			_ttl = Color3.fromRGB(255, 85, 0),
+			_bps = Color3.fromRGB(255, 85, 0),
+			_lblt = Color3.fromRGB(255, 85, 0),
+			_te = tostring("\240\159\142\131")
+		},
+
+		-- 1 December 2023
+		_christmas_mode = {
+			_ttl = Color3.fromRGB(255, 0, 0),
+			_bps = Color3.fromRGB(255, 0, 0),
+			_lblt = Color3.fromRGB(255, 0, 0),
+			_te = tostring("\240\159\142\133")
+		}
+	},
+
+	_discord = {
+		_nwl = "eRzY9DYTch"
+	}
+}
